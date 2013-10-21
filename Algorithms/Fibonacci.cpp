1b251a61dfba19d11a5806a1525401416146b4f9
ashik992 <ashik_992@yahoo.com>
2013-10-21 21:39:56 +0600

    Add Algorithms

---
+++ Algorithms/Fibonacci.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+int fib(int n){
+	int number;
+	if(n <= 1){
+		return n;
+	}else{
+		return fib(n-1) + fib(n-2);
+		
+	}
+}
+
+int main(){
+	int st = 0;
+	int nd = 1;
+	int fi;
+	
+	int n =1;
+	
+	cout<<"The "<<n<<"th number using Recursion : "<<fib(n)<<endl;
+	
+	if(n<=1){
+		fi = n;
+	}else{
+		for(int i=0; i<=n-2; i++){
+			fi = st + nd;
+			st = nd;
+			nd = fi;
+		}
+	}
+	
+	cout<<"The "<<n<<"th number using Iteration : "<<fi<<endl;
+
+	return 0;
+}
---
+++ Algorithms/Max_Min.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int max1, max2;
+int min1, min2;
+int mid;
+int A[]={3,45,30,57,1,6};
+
+
+void devNConc(int lft, int rit, int *max, int *min){
+	if(rit-lft <= 1){
+        if(A[lft]<A[rit]){
+			*max = A[rit];
+			*min = A[lft];
+			
+		}else{
+			*max = A[lft];
+			*min = A[rit];
+			
+		}
+		
+	}else{
+		mid = (lft + rit)/2;
+		devNConc(lft, mid, &max1, &min1);
+		devNConc(mid+1, rit, &max2, &min2);
+
+		if(max1>max2){
+			*max = max1;
+			
+		}else{
+			*max = max2;
+			
+		}
+		
+		if(min1<min2){
+			*min = min1;
+			
+		}else{
+			*min = min2;
+			
+		}
+		
+	}
+
+}
+
+int main(){
+	int maxF;
+	int minF;
+	
+	devNConc(0, 5, &maxF, &minF);
+	cout<<"Max :"<<maxF<<endl<<"Min :"<<minF;
+}
