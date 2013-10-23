4d6d12ec03bf8933c0bce41063cfc4565a555cf7
ashik992 <ashik_992@yahoo.com>
2013-10-23 12:08:46 +0600

    Add the stucture of class Sales

---
+++ Object_Oriented_Programming_I/Lab_Practice_2/Sales.java
@@ -0,0 +1,48 @@
+class Sales{
+	float totalSalesAmount;
+	float salesAmount;
+	int productCounter;
+	float unitBuyPrice;
+	float unitSellPrice;
+	static float totalProfit;
+	
+	//@ Set methods 
+	void addProduct(int quantity){
+		
+		
+	}
+	
+	void doSells(int quantitySold){
+		
+	}
+	
+	void setUnitBuyPrice(float newPrice){
+		
+	}
+	
+	void setUintSellPrice(float newPrice){
+		
+	}
+	
+	
+	//@ Show or get methods
+	void showBills(){
+		
+	}
+	
+	void showTotalSells(){
+		
+	}
+	
+	void showTotalProfits(){
+		
+	}
+	
+
+	
+	//@main methods
+	
+	public static void main(String args[]){
+		System.out.println("Sales class is working");
+	}
+}
