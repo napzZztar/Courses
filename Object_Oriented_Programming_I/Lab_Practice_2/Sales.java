5bc722f393ef27a45acf2bd4c06d9e6647ff91d4
ashik992 <ashik_992@yahoo.com>
2013-10-23 14:14:00 +0600

    Update Saless

--- Object_Oriented_Programming_I/Lab_Practice_2/Sales.java
+++ Object_Oriented_Programming_I/Lab_Practice_2/Sales.java
@@ -2,40 +2,48 @@ class Sales{
 	float totalSalesAmount;
 	float salesAmount;
 	int productCounter;
+	int totalSells;
 	float unitBuyPrice;
 	float unitSellPrice;
 	static float totalProfit;
 	
 	//@ Set methods 
 	void addProduct(int quantity){
-		
+		this.productCounter += quantity;
 		
 	}
 	
 	void doSells(int quantitySold){
-		
+		this.productCounter -= quantitySold;
+		this.salesAmount = unitSellPrice * quantitySold;
+		this.totalSalesAmount += salesAmount;
+		this.totalSells += quantitySold;
 	}
 	
 	void setUnitBuyPrice(float newPrice){
-		
+		this.unitBuyPrice = newPrice;
 	}
 	
 	void setUintSellPrice(float newPrice){
+		this.unitSellPrice = newPrice;
 		
 	}
 	
 	
 	//@ Show or get methods
 	void showBills(){
-		
+		System.out.println("Unit sell price     : "+this.unitSellPrice);
+		System.out.println("Sales Amount        : "+this.salesAmount);
 	}
 	
 	void showTotalSells(){
-		
+		System.out.println("Total Sells quntity : "+this.totalSells);
+		System.out.println("Total Sells amount  : "+this.totalSalesAmount)
 	}
 	
 	void showTotalProfits(){
-		
+		this.totalProfit += (this.unitSellPrice - this.unitBuyPrice) * this.salesAmount;
+		System.out.println("Total Profit        : "+this.totalProfit);
 	}
 	
 
