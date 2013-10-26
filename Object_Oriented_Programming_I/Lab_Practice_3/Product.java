813764fdeb5599ea17525d39a74a4fd3603c7d9e
ashik992 <ashik_992@yahoo.com>
2013-10-27 00:53:11 +0600

    Done with all the methods in Product

--- Object_Oriented_Programming_I/Lab_Practice_3/Product.java
+++ Object_Oriented_Programming_I/Lab_Practice_3/Product.java
@@ -5,6 +5,30 @@ class Product{
 	String expDate;
 	double unitProdCost;
 	double unitSellPrice;
+	static int prodNumber;
+	
+	//@Constructors
+	Product(){
+		this.prodId = 0;
+		this.prodName = "";
+		this.manuDate = "";
+		this.expDate = "";
+		this.unitProdCost = 0.0;
+		this.unitSellPrice = 0.0;
+		//prodNumber++;
+		
+	}
+
+	Product(int prodId,  String prodName,  String manuDate,  String expDate,  double unitProdCost,  double unitSellPrice){
+		this.prodId = prodId;
+		this.prodName = prodName;
+		this.manuDate = manuDate;
+		this.expDate = expDate;
+		this.unitProdCost = unitProdCost;
+		this.unitSellPrice = unitSellPrice;
+		prodNumber++;
+	}
+
 	
 	//@ Set methods
 	void setProdId(int prodId){
@@ -30,6 +54,10 @@ class Product{
 	void setUnitSellPrice(double unitSellPrice){
 		this.unitSellPrice = unitSellPrice;
 	}
+	
+	static void setProdNumber(int prodNum){
+		prodNumber = prodNum;
+	}
 
 	
 	//@ get methods
@@ -53,10 +81,13 @@ class Product{
 		return this.unitProdCost;
 	}
 	
-	double get(){
+	double getSellPrice(){
 		return this.unitSellPrice;
 	}
 
+	int getProdNumber(){
+		return prodNumber;
+	}
 	
 	//@ Main method
 	public static void main(String args[]){
