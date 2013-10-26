691561ad186d598a544ff3e7d4546e44fe450b34
ashik992 <ashik_992@yahoo.com>
2013-10-27 01:45:13 +0600

    Add some python examples

---
+++ Operating_System_I/Python/example3.py
@@ -0,0 +1,14 @@
+my_name = 'Ashik Ahmed'
+my_age = 21 #not a lie
+my_height = 68 #inch
+my_weight = 57 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk aobut %s" %my_name
+print "He's %d inch tall" % my_height
+print "He's %d kg heavy" %my_weight
+print "Atually that's not too heavy."
+print "He's got %s eye and %s hair" %(my_eyes, my_hair)
+print "If i add %d, %d and %d I get %d" %(my_age, my_height, my_weight, my_age + my_height + my_weight)
