d2c924947e1582e69509213891c53bb6763c7e2a
ashik992 <ashik_992@yahoo.com>
2013-11-03 15:51:16 +0600

    Add Assignment on Selection Statements

---
+++ Object_Oriented_Programming_I/Assignment_Selection_Statements/Employee.java
@@ -0,0 +1,17 @@
+class Employee{
+   int empId;
+   String name;
+   String dept;
+   String designation;
+   String joinDate;
+   String dateOfBirth;
+   String contactNo;
+   String email;
+   int basicSalary;
+   int commission;
+   int totalSalary;
+   int medicalAllowance;
+   int houseRent;
+
+
+}
