21add14b1e734673249529426411adcaca385ebc
ashik992 <ashik_992@yahoo.com>
2013-11-03 16:50:19 +0600

    Done with the Show method

--- Object_Oriented_Programming_I/Assignment_Selection_Statements/Employee.java
+++ Object_Oriented_Programming_I/Assignment_Selection_Statements/Employee.java
@@ -7,11 +7,49 @@ class Employee{
    String dateOfBirth;
    String contactNo;
    String email;
-   int basicSalary;
-   int commission;
-   int totalSalary;
-   int medicalAllowance;
-   int houseRent;
+   double basicSalary;
+   double commission;
+   double totalSalary;
+   double totalSalesAmount;
+   double medicalAllowance;
+   double houseRent;
 
+   Employee(String name, int empId, String dept, String designation, double totalSalesAmount){
+     this.name = name;
+     this.empId = empId;
+     this.dept = dept;
+     this.designation = designation;
+     this.totalSalesAmount = totalSalesAmount;
 
+     if(this.dept=="Sales"){
+
+     }
+
+   }
+
+   void showEmpInfo(){
+      System.out.ptintln("------------------------------")
+      System.out.println("Employee Id   : "+this.name);
+      System.out.println("Employee Name : "+this.empId);
+      System.out.println("Departments   : "+this.dept);
+      System.out.println("Designation   : "+this.designation);
+      System.out.println(" : "+this.joinDate);
+      System.out.println(" : "+this.dateOfBirth);
+      System.out.println(" : "+this.contactNo);
+      System.out.println(" : "+this.email);
+      System.out.println("Basic         : "+this.basicSalary);
+      System.out.println("House Rent    : "+this.houseRent);
+      System.out.println("Medical       : "+this.medicalAllowance);
+      System.out.println("              : "+this.commission);
+      System.out.println("Commission    : "+this.totalSalesAmount)
+      System.out.ptintln("------------------------------")
+      System.out.println("Total Salary  : "+this.totalSalary);
+
+   }
+
+   public static void main(String args[]){
+      Employee emp1 = new Employee("Ashik Ahmed", 1220, "CSE", "Manager");
+      emp1.showEmpInfo();
+
+   }
 }
