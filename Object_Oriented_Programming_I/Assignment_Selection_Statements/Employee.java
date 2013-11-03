16dd2f8a3fad3796b5ff54b121e2c6c823d498d5
ashik992 <ashik_992@yahoo.com>
2013-11-04 01:30:33 +0600

    Get methods done

--- Object_Oriented_Programming_I/Assignment_Selection_Statements/Employee.java
+++ Object_Oriented_Programming_I/Assignment_Selection_Statements/Employee.java
@@ -1,20 +1,21 @@
 class Employee{
-   int empId;
-   String name;
-   String dept;
-   String designation;
-   String joinDate;
-   String dateOfBirth;
-   String contactNo;
-   String email;
-   double basicSalary;
-   double commission;
-   double totalSalary;
-   double totalSalesAmount;
-   double medicalAllowance;
-   double houseRent;
+   private int empId;
+   private String name;
+   private String dept;
+   private String designation;
+   private String joinDate;
+   private String dateOfBirth;
+   private String contactNo;
+   private String email;
+   private double basicSalary;
+   private double commission;
+   private double totalSalary;
+   private double totalSalesAmount;
+   private double medicalAllowance;
+   private double houseRent;
 
-   Employee(String name, int empId, String dept, String designation, double totalSalesAmount){
+   //@ constructors.
+   public Employee(String name, int empId, String dept, String designation, double totalSalesAmount){
      this.name = name;
      this.empId = empId;
      this.dept = dept;
@@ -27,8 +28,52 @@ class Employee{
 
    }
 
-   void showEmpInfo(){
-      System.out.ptintln("------------------------------")
+   //@get methods
+   void setname(String name){
+      this.name = name;
+   }
+   void setdept(String dept){
+      this.dept = dept;
+   }
+   void setdesignation(String designation){
+      this.designation = designation;
+   }
+   void setjoinDate(String joinDate){
+      this.joinDate = joinDate;
+   }
+   void setdateOfBirth(String dateOfBirth){
+      this.dateOfBirth = dateOfBirth;
+   }
+   void setcontactNo(String contactNo){
+      this.contactNo = contactNo;
+   }
+   void setemail(String email){
+      this.email = email;
+   }
+   void setbasicSalary(double basicSalary){
+      this.basicSalary = basicSalary;
+   }
+   void setcommission(double commission){this.commission = commission;
+   }
+   void settotalSalary(double totalSalary){
+      this.totalSalary = totalSalary;
+   }
+   void settotalSalesAmount(double totalSalesAmount){
+      this.totalSalesAmount = totalSalesAmount;
+   }
+   void setmedicalAllowance(double medicalAllowance){
+      this.medicalAllowance = medicalAllowance;
+   }
+   void sethouseRent(double houseRent){
+      this.houseRent = houseRent;
+   }
+
+
+
+
+//{{{
+   public void showEmpInfo(){
+      System.out.println("------------------------------");
       System.out.println("Employee Id   : "+this.name);
       System.out.println("Employee Name : "+this.empId);
       System.out.println("Departments   : "+this.dept);
@@ -41,14 +86,14 @@ class Employee{
       System.out.println("House Rent    : "+this.houseRent);
       System.out.println("Medical       : "+this.medicalAllowance);
       System.out.println("              : "+this.commission);
-      System.out.println("Commission    : "+this.totalSalesAmount)
-      System.out.ptintln("------------------------------")
+      System.out.println("Commission    : "+this.totalSalesAmount);
+      System.out.println("------------------------------");
       System.out.println("Total Salary  : "+this.totalSalary);
 
-   }
+   }//}}}
 
    public static void main(String args[]){
-      Employee emp1 = new Employee("Ashik Ahmed", 1220, "CSE", "Manager");
+      Employee emp1 = new Employee("Ashik Ahmed", 1220, "Sales", "Manager", 1200);
       emp1.showEmpInfo();
 
    }
