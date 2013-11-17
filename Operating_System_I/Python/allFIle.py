3ff0bdb165b6eb7c4cdf07a251bdbbb9ee94ec8c
ashik992 <ashik.992@gmail.com>
2013-11-17 23:39:14 +0600

    Added allFile.py to count all file in directory

---
+++ Operating_System_I/Python/allFIle.py
@@ -0,0 +1,32 @@
+from os.path import exists
+from sys import argv
+import glob
+
+script, path, outFile = argv
+path = "%s*.cpp" %path
+filePth = glob.glob(path)
+print "Entered path : "+path
+
+if exists(outFile):
+   print "Accesing output file"
+else:
+   print "Output file DNE"
+   print "Press Enter to create"
+   print "Press CTRL+C to cancle"
+   raw_input()
+   print "File created!"
+   
+outFile = open(outFile, 'w+')
+outFile.write("File Name              -            No of LIne\n")
+
+print "Counting line no...."
+
+for file in filePth:
+   if file[-4:] != ".zip":
+      with open(file) as fin:
+         lines = sum(1 for line in fin)
+      str = "%s     -   %d\n" %(file, lines)
+      outFile.write(str)
+print "All DONE!!"
+
+outFile.close()
