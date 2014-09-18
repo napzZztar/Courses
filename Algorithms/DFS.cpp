2cdb072e0a42bde8adffb15cb58096a62296a09d
ashik992 <ashik.992@gmail.com>
2014-09-18 14:09:33 +0600

    Just a check

--- Algorithms/DFS.cpp
+++ Algorithms/DFS.cpp
@@ -10,73 +10,73 @@ char vColor[7];
 char aColor[7][7];
 
 void DFS_Visit(int u){
-   vColor[u] = 'G';
-   start[u] = ++times;
-   for(int i=1; i<7; i++){
-      if(graph[u][i] == 1){
-         if(vColor[i] == 'W'){
-            DFS_Visit(i);
-            aColor[u][i] = 'T';
-            parent[i] = u;
-         }else if(start[u] < start[i]){
-            aColor[u][i] = 'F';
-         }else{
-            aColor[u][i] = 'B';
-         }
-      }
-   }
-   vColor[u] = 'B';
-   finish[u] = ++times;
-   cout<<"==========="<<endl;
-   cout<<"Vertex : "<<u<<endl;
-   cout<<"Start  : "<<start[u]<<endl;
-   cout<<"Finish : "<<finish[u]<<endl;
+    vColor[u] = 'G';
+    start[u] = ++times;
+    for(int i=1; i<7; i++){
+        if(graph[u][i] == 1){
+            if(vColor[i] == 'W'){
+                DFS_Visit(i);
+                aColor[u][i] = 'T';
+                parent[i] = u;
+            }else if(start[u] < start[i]){
+                aColor[u][i] = 'F';
+            }else{
+                aColor[u][i] = 'B';
+            }
+        }
+    }
+    vColor[u] = 'B';
+    finish[u] = ++times;
+    cout<<"==========="<<endl;
+    cout<<"Vertex : "<<u<<endl;
+    cout<<"Start  : "<<start[u]<<endl;
+    cout<<"Finish : "<<finish[u]<<endl;
 }
 
 void DFS(){
-   times = 0;
-   for(int i=0; i<7; i++) vColor[i] = 'W';
-   for(int i=1; i<7; i++){
-      if(vColor[i] == 'W'){
-         DFS_Visit(i);
-      }
-   }
+    times = 0;
+    for(int i=0; i<7; i++) vColor[i] = 'W';
+    for(int i=1; i<7; i++){
+        if(vColor[i] == 'W'){
+            DFS_Visit(i);
+        }
+    }
 }
 
 void inputGraph(){
-   /*
-      for(int i=1; i<7; i++){
-      for(int j=1; j<7; j++){
-      cout<<i<<" -> "<<j<<" :";
-      cin>>graph[i][j];
-      }
-      }*/
-   //Just a little short cut.
-   graph[1][2] = 1;
-   graph[1][4] = 1;
-   graph[2][3] = 1;
-   graph[3][4] = 1;
-   graph[4][2] = 1;
-   graph[5][3] = 1;
-   graph[5][6] = 1;
-   graph[6][6] = 1;
+    /*
+       for(int i=1; i<7; i++){
+       for(int j=1; j<7; j++){
+       cout<<i<<" -> "<<j<<" :";
+       cin>>graph[i][j];
+       }
+       }*/
+    //Just a little short cut.
+    graph[1][2] = 1;
+    graph[1][4] = 1;
+    graph[2][3] = 1;
+    graph[3][4] = 1;
+    graph[4][2] = 1;
+    graph[5][3] = 1;
+    graph[5][6] = 1;
+    graph[6][6] = 1;
 }
 
 void showGraph(){
-   cout<<"Edges of the grapth are : "<<endl;
-   for(int i=1; i<7; i++){
-      for(int j=1; j<7; j++){
-         if(graph[i][j]) cout<<i<<" -> "<<j<<" : "<<aColor[i][j]<<endl;
-      }
-   }
+    cout<<"Edges of the grapth are : "<<endl;
+    for(int i=1; i<7; i++){
+        for(int j=1; j<7; j++){
+            if(graph[i][j]) cout<<i<<" -> "<<j<<" : "<<aColor[i][j]<<endl;
+        }
+    }
 }
 
 int main(){
-   for(int i=0; i<7;i++) for(int j=0; j<7; j++) aColor[i][j] = 'N';
-   inputGraph();
-   showGraph();
-   DFS();
-   showGraph();
-   for(int i=1; i<7; i++) cout<<parent[i]<<", ";
-   return 0;
+    for(int i=0; i<7;i++) for(int j=0; j<7; j++) aColor[i][j] = 'N';
+    inputGraph();
+    showGraph();
+    DFS();
+    showGraph();
+    for(int i=1; i<7; i++) cout<<parent[i]<<", ";
+    return 0;
 }
--- Algorithms/Fibonacci.cpp
+++ Algorithms/Fibonacci.cpp
@@ -3,35 +3,34 @@ using namespace std;
 
 
 int fib(int n){
-   int number;
-   if(n <= 1){
-      return n;
-   }else{
-      return fib(n-1) + fib(n-2);
+    if(n <= 1){
+        return n;
+    }else{
+        return fib(n-1) + fib(n-2);
 
-   }
+    }
 }
 
 int main(){
-   int st = 0;
-   int nd = 1;
-   int fi;
+    int st = 0;
+    int nd = 1;
+    int fi;
 
-   int n =1;
+    int n =1;
 
-   cout<<"The "<<n<<"th number using Recursion : "<<fib(n)<<endl;
+    cout<<"The "<<n<<"th number using Recursion : "<<fib(n)<<endl;
 
-   if(n<=1){
-      fi = n;
-   }else{
-      for(int i=0; i<=n-2; i++){
-         fi = st + nd;
-         st = nd;
-         nd = fi;
-      }
-   }
+    if(n<=1){
+        fi = n;
+    }else{
+        for(int i=0; i<=n-2; i++){
+            fi = st + nd;
+            st = nd;
+            nd = fi;
+        }
+    }
 
-   cout<<"The "<<n<<"th number using Iteration : "<<fi<<endl;
+    cout<<"The "<<n<<"th number using Iteration : "<<fi<<endl;
 
-   return 0;
+    return 0;
 }
