14d13a63e252195a89f63a23bb40ce4821ec0f6c
Ashik Ahmed <ashik.992@gmail.com>
2015-01-18 20:57:56 +0600

    Done

--- Computer_Graphics/installOpenGl.sh
+++ Computer_Graphics/installOpenGl.sh
@@ -2,8 +2,11 @@ sudo apt-get install build-essential
 sudo apt-get install freeglut3-dbg freeglut3
 sudo apt-get install libglewmx-dev glew-utils libgle3-dbg libglewmx-dbg libglew-dev libglewmx1.10 libglew1.10
 
-sudo cp /usr/lib/x86_64-linux-gnu/libglut.a /usr/lib
+sudo cp /usr/lib/x86_64-linux-gnu/libglut.a 
 sudo cp /usr/lib/x86_64-linux-gnu/libglut.so /usr/lib
 sudo cp /usr/lib/x86_64-linux-gnu/libglut.so.3 /usr/lib
 sudo cp /usr/lib/x86_64-linux-gnu/libglut.so.3.9.0 /usr/lib
+
+cd /usr/lib
+udo chown $USER:$USER libglut.*
 
