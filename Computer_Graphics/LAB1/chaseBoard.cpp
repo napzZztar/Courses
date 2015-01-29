c2e2be1977e1bf0e10874aa3c928d05ea5d3ca2b
Ashik Ahmed <ashik.992@gmail.com>
2015-01-30 03:42:02 +0600

    Done!! Now in need to know how to play Chase.. :P :/

--- Computer_Graphics/LAB1/chaseBoard.cpp
+++ Computer_Graphics/LAB1/chaseBoard.cpp
@@ -2,7 +2,7 @@
 #include <GL/glut.h>
 
 void myInit();
-void DrawSquare();
+void DrawSquare(int x, int y);
 void Chase();
 
 int main(int argc, char** argv){
@@ -11,6 +11,41 @@ int main(int argc, char** argv){
     glutInitWindowSize(800, 800);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Chase");
-
+    glutDisplayFunc(Chase);
+    myInit();
+    glutMainLoop();
     return 0;
+}
+
+void DrawSquare(int x, int y){
+    glBegin(GL_TRIANGLE_STRIP);
+    glVertex2i(x+100, y);
+    glVertex2i(x, y);
+    glVertex2i(x+100, y+100);
+    glVertex2i(x, y+100);
+    glEnd();
+}
+
+void Chase(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3i(0, 0, 0);
+    glPointSize(1.0);
+
+    for (int i = 0; i < 800; i+=100) {
+        for (int j=0; j < 800; j+=100) {
+            if( ((i/100)%2==0) && ((j/100)%2==0) )
+                DrawSquare(i,j);
+            else if( ((i/100)%2!=0) && ((j/100)%2!=0) )
+                DrawSquare(i,j);
+        }
+    }
+
+    glFlush();
+}
+
+void myInit(){
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 800, 0, 800);
 }
