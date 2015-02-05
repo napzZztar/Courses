65bcdf22fc1939535fb74018cd6d3dfb2765ddb4
Ashik Ahmed <ashik.992@gmail.com>
2015-02-05 21:11:24 +0600

    Basic skeleton

---
+++ Statstics/DataRepresent.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <GL/glut.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+ 
+//******************************
+//** Grapics Functions *********
+//******************************
+void drawGraph();
+void myInit();
+void drawRect();
+void myDisplay();
+
+//******************************
+//** Statstics Functions *******
+//******************************
+void input();
+void createTable();
+
+int main(){
+
+    return 0;
+}
