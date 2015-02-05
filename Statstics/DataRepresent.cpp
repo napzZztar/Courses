2cde7f02df977f09c1835afefe5f36b3cfff6c66
Ashik Ahmed <ashik.992@gmail.com>
2015-02-06 00:21:25 +0600

    Infrom file and table

--- Statstics/DataRepresent.cpp
+++ Statstics/DataRepresent.cpp
@@ -2,9 +2,23 @@
 #include <GL/glut.h>
 #include <iostream>
 #include <math.h>
+#include <fstream>
 
 using namespace std;
- 
+
+struct tab{
+    int lower;
+    int higher;
+    int freaqu;
+};
+
+int h;
+int fn;
+int noOfData = 0;
+float data[500] = {-1};
+tab table[50];
+
+
 //******************************
 //** Grapics Functions *********
 //******************************
@@ -20,6 +34,9 @@ void input();
 void createTable();
 
 int main(int argv, char** argc){
+    input();
+    createTable();
+
     glutInit(&argv,argc);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 800);
@@ -31,6 +48,11 @@ int main(int argv, char** argc){
 
     return 0;
 }
+
+
+//******************************
+//** Grapics Functions *********
+//******************************
 
 void drawGraph(){
     glLineWidth(2.5);
@@ -77,8 +99,8 @@ void drawRect(int x, int l){
 
     glBegin(GL_POLYGON);
     glVertex2d(x, 0);
-    glVertex2d(x+50, 0);
-    glVertex2d(x+50, l);
+    glVertex2d(x+h*5, 0);
+    glVertex2d(x+h*5, l);
     glVertex2d(x, l);
     glEnd();
 }
@@ -95,13 +117,66 @@ void myDisplay(){
     glColor3i(0, 0, 0);
     glPointSize(4.0);
 
-//******************************
-//* All the things to draw *****
-//******************************
-
+    //* All the things to draw *****
     drawGraph();
     drawRect(5, 10);
 
     glFlush();
 
+}
+
+
+//******************************
+//** Statstics Functions *******
+//******************************
+
+void input(){
+    fstream myfile("input.txt", ios_base::in);
+
+    int i=0;
+    while (myfile >> data[i++])
+        noOfData++;
+}
+
+void createTable(){
+    float max = data[0];
+    float min = data[0];
+
+    //find the max and min values
+    for (int i = 0; i < noOfData; i++) {
+        if(data[i] < min)
+            min = data[i];
+
+        if(data[i] > max)
+            max = data[i];
+    }
+
+    h = ceil((max-min)/(1+3.3*log10(noOfData)));
+    
+    table[0].lower = min;
+    table[0].higher = min+h;
+
+    for (fn = 1; ; fn++) {
+        table[fn].lower = table[fn-1].higher;
+        table[fn].higher = table[fn].lower +h;
+        table[fn].freaqu = 0;
+
+        if(table[fn].higher >= max){
+            fn++;
+            break;
+        }
+    }
+
+    for (int i = 0; i < noOfData; i++) {
+        for (int j = 0; j < fn; j++) {
+            if(data[i] <= table[j].higher){
+                table[j].freaqu++;
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < fn; i++) {
+        printf("%d-%d\t%d\n", table[i].lower, table[i].higher, table[i].freaqu);
+    }
 }
