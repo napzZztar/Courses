7eeab0895d18f2d1ea78080799dd839f053f4a10
Ashik Ahmed <ashik.992@gmail.com>
2015-02-06 06:19:53 +0600

    A "Perfect" circle

---
+++ Computer_Graphics/misc/circle.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <GL/glut.h>
+#include <math.h>
+
+void myDisplay();
+void glutInit();
+
+int main(int argc, char **argv){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Circle");
+    glutDisplayFunc(myDisplay);
+    glutInit();
+    glutMainLoop();
+    
+    return 0;
+}
+
+
+void myDisplay(){
+    int r = 200;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glPointSize(2.0);
+    
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2d(0, 0);
+    for (float i = 0; i<=((360*3.1416)/180); i+=0.01) {
+        glVertex2f((r*cos(i)), (r*sin(i)));
+    }
+
+    glEnd();
+    glFlush();
+}
+
+void glutInit(){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-400, 400, -400, 400);
+}
