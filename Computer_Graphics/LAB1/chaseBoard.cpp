aa912d734a5d5777e23ffc47d5ba5ebe1667074b
Ashik Ahmed <ashik.992@gmail.com>
2015-02-08 20:41:41 +0600

    Quads is a better choice

--- Computer_Graphics/LAB1/chaseBoard.cpp
+++ Computer_Graphics/LAB1/chaseBoard.cpp
@@ -9,30 +9,30 @@
 
 void myInit();
 void DrawSquare(int x, int y);
-void Chesse();
+void Chess();
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 800);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Chesse");
-    glutDisplayFunc(Chesse);
+    glutCreateWindow("Chess");
+    glutDisplayFunc(Chess);
     myInit();
     glutMainLoop();
     return 0;
 }
 
 void DrawSquare(int x, int y){
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2i(x+100, y);
+    glBegin(GL_QUADS);
     glVertex2i(x, y);
-    glVertex2i(x+100, y+100);
     glVertex2i(x, y+100);
+    glVertex2i(x+100, y+100);
+    glVertex2i(x+100, y);
     glEnd();
 }
 
-void Chesse(){
+void Chess(){
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3f(0, 0, 0);
     glPointSize(1.0);
