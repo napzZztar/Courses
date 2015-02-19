a8fbbc2b681b0816bb5a41d25695b32bc5ac1059
Ashik Ahmed <ashik.992@gmail.com>
2015-02-19 21:06:22 +0600

    solve() should work as expected

--- Artificial_Intelligence/misc/eitQueen.cpp
+++ Artificial_Intelligence/misc/eitQueen.cpp
@@ -14,9 +14,27 @@ int board[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
                    {0, 0, 0, 0, 0, 0, 0, 0},
                    {0, 0, 0, 0, 0, 0, 0, 0}};
 
+int placeQueen(int row);
+void solve();
 
 int main(){
     
     return 0;
 }
 
+void solve(){
+    int temp;
+    int lastQueen;
+    for (int i = 0; i < 8; i++) {
+        if ((temp = placeQueen(i)) == 9) {
+            board[--i][lastQueen] = 2;
+        }else{
+            lastQueen = temp;
+        }
+    }
+}
+
+int placeQueen(int row){
+    
+    return false;
+}
