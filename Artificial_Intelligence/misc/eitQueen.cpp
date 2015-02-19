097bf837f63ec8d405793f037e49c3597bdd7ed2
Ashik Ahmed <ashik.992@gmail.com>
2015-02-20 00:43:41 +0600

    Now all I need to do is to get the diagonal check

--- Artificial_Intelligence/misc/eitQueen.cpp
+++ Artificial_Intelligence/misc/eitQueen.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
 using namespace std;
- 
+
 // 0 = not tried yet
 // 1 = queen
 // 2 = won't work
 int board[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0},
-                   {0, 0, 0, 0, 0, 0, 0, 0}};
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}};
 
 int placeQueen(int row);
 void solve();
@@ -21,51 +21,56 @@ void print();
 
 int main(){
     solve();
-    print();
-    
+
     return 0;
 }
 
 void solve(){
     int temp;
-    int lastQueen;
-    for (int i = 0; i < 8; i++) {
-        if ((temp = placeQueen(i)) == 9) {
-            board[--i][lastQueen] = 2;
+    int lastCol;
+    for (int i = 0; i < 8;) {
+        cout<<i<<endl<<endl;
+        temp = placeQueen(i);
+
+        if (temp == 9) {
+            board[--i][lastCol] = 2;
+            for (int j = i; j < 8; j++) {
+                for (int k = lastCol+1; k < 8; k++) {
+                    board[j][k] = 0;
+                }
+            }
         }else{
-            lastQueen = temp;
+            lastCol= temp;
+            i++;
         }
+
+        print();
+        cout<<endl<<"=============================="<<endl<<endl;
     }
 }
 
 int placeQueen(int row){
     bool possible;
-    for (int col = 0; col < 8; col++) {
-        if(board[row][col] != 2){
-            for (int j = row; j < 8; j++) {
-                for (int k = col; k < 8; k++) {
-                    if ((board[row][j]!=1) || (board[j][col]!=1) || (board[j][k]!=1)) {
-                        possible = true;
-                    }else
-                        possible = false;
-                }
-            }
-        }
-    }
+    int col;
 
-    if(possible){
-        for (int col = 8; col >= 0; col--) {
-            if(board[row][col] != 2){
-                for (int j = row; j >= 0; j--) {
-                    for (int k = col; k >= 0; k--) {
-                        if ((board[row][j]!=1) || (board[j][col]!=1) || (board[j][k]!=1)) {
-                            board[row][col] = 1;
-                            return col;
-                        }
-                    }
+    for (col = 0; col < 8; col++) {
+        possible = true;
+        if(board[row][col] != 2){
+            for (int j = 0; j < 8; j++) {
+                if ((board[row][j]==1) || (board[j][col]==1)){
+                    possible = false;
+                    break;
                 }
+                if(!possible)
+                    break;
+            }
+
+            if(possible){
+                board[row][col] = 1;
+                return col;
             }
         }
+
     }
 
     return 9;
@@ -76,6 +81,6 @@ void print(){
         for (int j = 0; j < 8; j++) {
             cout<<board[i][j]<<" - ";
         }
-        cout<<endl;
+        cout<<endl<<endl;
     }
 }
