0c9f6831aee2550151fac9c74c534109f1f467a6
Ashik Ahmed <ashik.992@gmail.com>
2015-03-16 14:44:09 +0600

    Added Circle.cpp

---
+++ Computer_Graphics/LAB4/circle.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int center_x;
+int center_y;
+int radious;
+
+void myDisplay();
+void glutInit();
+void drawCircle(int x,int y, int r);
+
+int main(int argc, char **argv){
+    cout<<"Enter th Coordinate of the center: ";
+    cin>>center_x>>center_y;
+    cout<<"Enter the radous: ";
+    cin>>radious;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(400, 400);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Circle");
+    glutDisplayFunc(myDisplay);
+    glutInit();
+    glutMainLoop();
+    
+    return 0;
+}
+
+void myDisplay(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
+    glPointSize(2.0);
+    drawCircle(center_x,center_y, radious);
+
+    glFlush();
+}
+
+void drawCircle(int x, int y, int r){
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2d(x, y);
+    for (float i = 0; i<=((361*3.1416)/180); i+=0.01) {
+        glVertex2f((x+r*cos(i)), (y+r*sin(i)));
+    }
+
+    glEnd();
+}
+
+void glutInit(){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-400, 400, -400, 400);
+}
