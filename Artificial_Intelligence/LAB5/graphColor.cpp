4cf4e88e6dbe53e0fa80b234564cfcc851410f9d
Ashik Ahmed <ashik.992@gmail.com>
2015-04-10 21:04:06 +0600

    Comments

--- Artificial_Intelligence/LAB5/graphColor.cpp
+++ Artificial_Intelligence/LAB5/graphColor.cpp
@@ -12,8 +12,8 @@ int color[arS] = {0};
 string colorName[6] = {" ", "Red", "Gre", "Blue", "Yellow", "Black"};
 
 void init();
-bool backTrack();
-void print();
+bool backTrack(); //assigns color to all nodes using with the help of backtrack
+void print(); //print the adjacency matrix with color value
 
 int main(){
     init();
@@ -21,7 +21,7 @@ int main(){
     cin>>m;
     m++;
 
-    if(backTrack()){
+    if(backTrack()){ //if backtrack returns false you need more colors
         print();
     }else{
         cout<<"Coloring not possible";
@@ -30,7 +30,7 @@ int main(){
     return 0;
 }
 
-void init(){
+void init(){ //assigns the value of the graph
     int edge;
     int s, d;
     cout<<"Please enter the number of edges: ";
@@ -41,7 +41,7 @@ void init(){
 
     for (int i = 0; i < edge; i++) {
         cin>>s>>d;
-        graph[s][d] = 1;
+        graph[s][d] = 1; //Not a directed graph
         graph[d][s] = 1;
     }
 
@@ -83,22 +83,22 @@ bool backTrack(){
 
     for (int i = 1; i < V; i++)
         for (int j = 1; j < m; j++)
-            possible[i][j] = 1;
+            possible[i][j] = 1; //making every color available for each node
 
     for (int i = 1; i < V; i++) {
         for (j = 1; j <= m; j++){
-            if(possible[i][j]){
-                color[i] = j;
-                possible[i][j] = 0;
+            if(possible[i][j]){ //if this color is still available for this node
+                color[i] = j; //assign j as the color 
+                possible[i][j] = 0; //make it unavailable
                 break;
-            }else if(j == m){
-                if(i==0)
+            }else if(j == m){ //if all 'm' colors are unavailable
+                if(i==0) //at the begining of the graph
                     return false;
-                i -= 2;
+                i -= 2; //back up a littile bit
             }
         }
 
-        for (int k = 1; k < V; k++) {
+        for (int k = 1; k < V; k++) { //making assigned color(j) unavailable for adjacent nodes
             if(graph[i][k])
                 possible[k][j] = 0;
         }
@@ -111,11 +111,11 @@ void print(){
 
     cout<<"\t";
     for (int i = 1; i < V; i++)
-        cout<<i<<"("<<colorName[color[i]]<<")\t";
+        cout<<i<<"("<<colorName[color[i]]<<")\t";  //if you get an error, your compiler's broken. Get a newer one.
     cout<<endl;
 
     for (int i = 1; i < V; i++) {
-        cout<<i<<"("<<colorName[color[i]]<<")\t";
+        cout<<i<<"("<<colorName[color[i]]<<")\t"; //if you get an error, your compiler's broken. Get a newer one.
         for (int j = 1; j < V; j++) {
             cout<<graph[i][j]<<"\t";
         }
